d2377e0d7bba614d48d5b9d6873ead594ed4e43b
Matt Black <github@mafro.net>
2019-04-13 06:27:44 +1000

    [bootstrap] Include bootstrap-org.sh script

---
+++ bootstrap/bin/bootstrap-org.sh
@@ -0,0 +1,127 @@
+#! /bin/bash -e
+
+USAGE='bootstrap-org.sh ORG_ID BOOTSTRAP_PROJECT KEY_FILEPATH [BILLING_ACCOUNT_ID]
+
+  ORG_ID               GCP organisation id
+  BOOTSTRAP_PROJECT   Terraform state project
+  KEY_FILEPATH         Local filepath in which to store service account key
+  BILLING_ACCOUNT_ID   Optionally link a billing account to the Terraform state project'
+
+if [[ $# -ne 3 && $# -ne 4 ]]; then
+	echo "$USAGE"
+	exit 1
+fi
+
+
+ORG_ID=$1
+BOOTSTRAP_PROJECT=$2
+KEY_FILEPATH="${3:-credentials.json}"
+BILLING_ACCOUNT_ID=$4
+
+# Display current gcloud user
+CURRENT_SA="$(gcloud config list account --format "value(core.account)")"
+printf "\\nYou are currently logged in as %s\\n\\n" "${CURRENT_SA}"
+
+if ! gcloud projects list | grep "${BOOTSTRAP_PROJECT}" &>/dev/null; then
+	printf "The foundation project (%s) does not current exist. Creating..\\n\\n" "${BOOTSTRAP_PROJECT}"
+
+	# Create FOUNDATION project
+	gcloud projects create "${BOOTSTRAP_PROJECT}" \
+		--organization "${ORG_ID}"
+
+	echo "Created project ${BOOTSTRAP_PROJECT}"
+else
+	echo "Project exists ${BOOTSTRAP_PROJECT}"
+fi
+
+if [[ -n $BILLING_ACCOUNT_ID ]]; then
+	# link billing to FOUNDATION state project
+	gcloud beta billing projects link "${BOOTSTRAP_PROJECT}" \
+		--billing-account "${BILLING_ACCOUNT_ID}"
+else
+	echo "Skipping billing link (not set)"
+fi
+
+for API in cloudbilling.googleapis.com \
+	cloudresourcemanager.googleapis.com \
+	compute.googleapis.com \
+	iam.googleapis.com; do
+
+	echo "Enabling ${API}"
+	gcloud services enable --project="${BOOTSTRAP_PROJECT}" ${API}
+done
+
+if ! gcloud iam service-accounts list --project="${BOOTSTRAP_PROJECT}" | grep terraform &>/dev/null; then
+
+	echo "Creating terraform service account"
+
+	# Create the account terraform will use
+	gcloud iam service-accounts create terraform \
+		--project="${BOOTSTRAP_PROJECT}" \
+		--display-name "Terraform"
+
+	# Create the service key for the above account
+	gcloud iam service-accounts keys create "${KEY_FILEPATH}" \
+		--project="${BOOTSTRAP_PROJECT}" \
+		--iam-account "terraform@${BOOTSTRAP_PROJECT}.iam.gserviceaccount.com"
+else
+	echo "Terraform service account already exists"
+fi
+
+if ! gsutil list -p "${BOOTSTRAP_PROJECT}" "gs://${BOOTSTRAP_PROJECT}"; then
+
+	echo "Creating terraform cloud storage bucket"
+
+	for BUCKET in ${BOOTSTRAP_PROJECT} ${BOOTSTRAP_PROJECT}-logs; do
+		# Create a Cloud Storage bucket for Terraform remote state & logs
+		gsutil mb -b on \
+			-l australia-southeast1 \
+			-p "${BOOTSTRAP_PROJECT}" \
+			"gs://${BUCKET}"
+
+		# Grant 'Storage Object Admin' role to the service account
+		gsutil iam ch \
+			"serviceAccount:terraform@${BOOTSTRAP_PROJECT}.iam.gserviceaccount.com:objectAdmin" \
+			"gs://${BUCKET}"
+	done
+else
+	echo "Terraform cloud storage bucket already exists"
+fi
+
+# Grant the following permissions at organization level for the service account:
+# - Billing Account User
+# - Editor
+# - Service Account Admin
+# - Service Account Key Admin
+# - Organization Admin
+# - Storage Admin
+
+CURRENT_ROLES="$(gcloud organizations get-iam-policy "${ORG_ID}" --format json | jq --raw-output ".bindings[] | select( .members[] | contains(\"terraform@${BOOTSTRAP_PROJECT}.iam.gserviceaccount.com\") ) | .role")"
+
+for ROLE in billing.user \
+	editor \
+	iam.serviceAccountAdmin \
+	iam.serviceAccountKeyAdmin \
+	resourcemanager.folderAdmin \
+	resourcemanager.organizationAdmin \
+	storage.admin; do
+
+	if [[ $CURRENT_ROLES != *"$ROLE"* ]]; then
+		gcloud organizations add-iam-policy-binding "${ORG_ID}" \
+			--member "serviceAccount:terraform@${BOOTSTRAP_PROJECT}.iam.gserviceaccount.com" \
+			--role "roles/${ROLE}" \
+			&>/dev/null
+
+		echo "$ROLE applied to terraform service account"
+	else
+		echo "$ROLE already set"
+	fi
+done
+
+cat << EOF
+
+*******
+* Terraform project ${BOOTSTRAP_PROJECT} and service account is setup.
+* Credentials have been written to ${KEY_FILEPATH}.
+*******
+EOF
