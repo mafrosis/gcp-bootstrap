698e71efa78123098e95f47b7c806c2c9242fe79
Matt Black <github@mafro.net>
2020-07-26 14:25:28 +1000

    [gcp-project] Only create DNS managed zone if var.dns_subdomain set

--- projects/modules/gcp-project/dns.tf
+++ projects/modules/gcp-project/dns.tf
@@ -1,6 +1,7 @@
 # create a DNS subzone for this project
 #
 locals {
+  create_zone   = var.dns_subdomain == null ? 0 : 1
   dns_subdomain = var.dns_subdomain == null ? var.project_name : var.dns_subdomain
 }
 
@@ -12,6 +13,8 @@ data google_dns_managed_zone root {
 
 # Create the project's sub-zone
 resource google_dns_managed_zone project {
+  count = local.create_zone
+
   project     = google_project.project.project_id
   name        = local.dns_subdomain
   dns_name    = format("%s.%s", local.dns_subdomain, data.google_dns_managed_zone.root.dns_name)
@@ -25,10 +28,12 @@ resource google_dns_managed_zone project {
 
 # Add nameservers for sub-zone to root zone on bootstrap project
 resource google_dns_record_set nameservers {
+  count = local.create_zone
+
   name         = format("%s.%s", local.dns_subdomain, data.google_dns_managed_zone.root.dns_name)
   project      = data.google_project.bootstrap_project.project_id
   managed_zone = data.google_dns_managed_zone.root.name
   type         = "NS"
   ttl          = 300
-  rrdatas      = google_dns_managed_zone.project.name_servers
+  rrdatas      = google_dns_managed_zone.project[0].name_servers
 }
